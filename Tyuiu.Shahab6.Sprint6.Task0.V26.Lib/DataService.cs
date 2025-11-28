2f44312d0012a2ab1da85f41848c0eed8d1feb74
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-28 17:17:51 +0500

    Task0

--- Tyuiu.Shahab6.Sprint6.Task0.V26.Lib/DataService.cs
+++ Tyuiu.Shahab6.Sprint6.Task0.V26.Lib/DataService.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.Shahab6.Sprint6.Task0.V26.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task0V26
     {
+        public DataService()
+        {
+        }
+
         public double Calculate(int x)
         {
             double result = (Math.Pow(x, 2) + 1) / (3 * x + 4);
