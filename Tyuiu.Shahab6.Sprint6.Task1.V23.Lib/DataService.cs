c1e8292a7d185a1819ab229de9499780fa5d72be
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-28 17:48:51 +0500

    Task1

---
+++ Tyuiu.Shahab6.Sprint6.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.Shahab6.Sprint6.Task1.V23.Lib
+{
+    public class DataService : ISprint6Task1V23
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int length = stopValue - startValue + 1;
+            double[] valueArray = new double[length];
+
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double term1 = Math.Sin(x);
+                double term2 = (2.0 * x) / 3.0;
+                double term3 = Math.Cos(x) * 4 * x;
+
+                double result = term1 + term2 - term3;
+                valueArray[count] = Math.Round(result, 2);
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.Shahab6.Sprint6.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.Shahab6.Sprint6.Task1.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+}
+}
+
+namespace Tyuiu.Shahab6.Sprint6.Task1.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+
+            double[] wait = {
+                -15.19, -10.63, -7.51, -3.40, 0.57,
+                0.42, 3.57, 6.07, 10.83, 15.92, 20.92
+            };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
